0a82276cdf8a0e64691ff82a0b1c8939534e2c70
mohit  Kumar Sharma <kaushikmohit54@hotmail.com>
2018-09-25 17:12:11 +0530

    2nd commit

---
+++ src/test/java/Pages/Login.java
@@ -0,0 +1,15 @@
+package Pages;
+
+import org.testng.annotations.Test;
+
+public class Login extends BaseTest {
+	
+	@Test
+	public void login1() {
+		driver.get("https://www.mailinator.com");
+		System.out.println("hi");
+		//hi
+			
+	}
+
+}
